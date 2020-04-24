60e782e85c8c11fecb2f3f672d706c719ac8eb7b
YongSub Kwak <yskwak@cest.co.kr>
2020-04-24 12:45:49 +0900

    feature

---
+++ Baekjoon_17613/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Baekjoon_17613
+{
+    class Program
+    {
+        public static Dictionary<string, int> lookuptable = new Dictionary<string, int>();
+
+        public static void Main(string[] args)
+        {
+            int nTest = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < nTest; i++)
+            {
+                var inputs = Console.ReadLine().Split(' ').Select(token => Convert.ToInt32(token)).ToList();
+
+                //int step = 0;
+
+                //while(inputs[0]!=0 && inputs[1] != 0)
+                //{
+                //    var tmax = getExpo(inputs[1]);
+
+                //    step += tmax;
+
+                //    inputs[0] = Math.Max(inputs[0] - (int)Math.Pow(2, tmax), 0);
+                //    inputs[1] = Math.Max(inputs[1] - (int)Math.Pow(2, tmax), inputs[0]);
+                //}
+                //Console.WriteLine(step);
+
+
+                int min = getMaxMinStep(inputs[0], inputs[1]);
+
+                Console.WriteLine(min);
+                //inputs[0] = Math.Max(inputs[0], (int)Math.Pow(2, getExpo(inputs[1])));
+                //inputs[1] = Math.Min(inputs[1], (int)Math.Pow(2, getExpo(inputs[0]) + 1));
+                //int min = getJump(inputs[0]);
+                //for (int j = inputs[0] + 1; j <= inputs[1]; j++)
+                //{
+                //    min = Math.Max(min, getJump(j));
+                //}
+
+                //Console.WriteLine(min);
+            }
+        }
+
+        public static int getMaxMinStep(int range_min, int range_max)
+        {
+            int step = 0;
+            int exp = getExpo(range_min + 1);
+
+            if (lookuptable.ContainsKey($"{range_min},{range_max}"))
+                return lookuptable[$"{range_min},{range_max}"];
+
+            if (range_max < range_min)
+                return step;
+            if (range_min == 0 && range_max == 0 )
+                return step;
+
+            //Console.WriteLine($"Check range [{range_min},{range_max}]");
+            for (; exp <= getExpo(range_max + 1); exp++)
+            {
+                int current_min = (int)Math.Pow(2, exp) - 1;   // inclusive
+                int current_max = (int)Math.Pow(2, exp + 1) - 2;   // exclusive
+
+                //Console.WriteLine($"when exp={exp} [{current_min},{current_max}]");
+
+                step = Math.Max(step,
+                    exp + getMaxMinStep(
+                        Math.Max(0, range_min - current_min),
+                        Math.Min(current_max - current_min, range_max - current_min)
+                        ));
+            }
+            lookuptable[$"{range_min},{range_max}"] = step;
+            return step;
+        }
+
+        public static int getJump(int target)
+        {
+            //int t = target;
+            int step = 0;
+            while (target > 0)
+            {
+                step += getExpo(target + 1);
+                target -= (int)Math.Pow(2, getExpo(target + 1)) - 1;
+            }
+            // Console.WriteLine($"{t} -> {step}");
+            return step;
+        }
+
+        public static int getExpo(int v)
+        {
+            int a = 0;
+            while(v > 1)
+            {
+                a++;
+                v = v >> 1;
+            }
+            return a;
+        }
+    }
+}
---
+++ TopologicalSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopologicalSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> vertices = new List<int>() { 0, 1, 2, 3, 4, 5 };
+            List<Tuple<int, int>> edges = new List<Tuple<int, int>>() {
+                new Tuple<int,int>(0,2), new Tuple<int,int>(0,3),
+                new Tuple<int,int>(1,3), new Tuple<int,int>(1,4),
+                new Tuple<int,int>(2,3), new Tuple<int,int>(2,5),
+                new Tuple<int,int>(3,5),
+                new Tuple<int,int>(4,5)
+            };
+
+            List<int> L = new List<int>(); // Empty list that will contain the result
+            List<int> S = new List<int>(); // Set of all vertices with no incoming edge
+
+            // find all vertices with no incoming edge
+            S = vertices.Where(vertex => edges.Where(edge => edge.Item2 == vertex).Count() == 0).ToList();
+            while (S.Count > 0)
+            {
+                //
+                var currentVertex = S[0]; // S[new Random().Next(S.Count)];
+                S.Remove(currentVertex);
+                L.Add(currentVertex);
+
+                // remove current vertex and its outgoing edges
+                vertices.Remove(currentVertex);
+                edges.RemoveAll(edge => edge.Item1 == currentVertex);
+
+                // find all vertices with no incoming edge again
+                S = vertices.Where(vertex => edges.Where(edge => edge.Item2 == vertex).Count() == 0).ToList();
+            }
+
+            if (edges.Count > 0)
+                Console.WriteLine("Cycle!");
+            else
+                for (int i = 0; i < L.Count; i++)
+                    Console.WriteLine(L[i]);
+
+        }
+
+    }
+}
