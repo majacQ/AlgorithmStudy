03fc92a35f8567404083e0706906b62a71af7f97
chickenchaser.kwak <chickenchaser.kwak@gmail.com>
2020-05-14 09:36:12 +0900

    문제풀이

---
+++ Baekjoon_14888/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon_14888
+{
+    class Program
+    {
+        public enum Operator
+        {
+            Plus,
+            Minus,
+            Multply,
+            Divide,
+            OperatorCounts
+        }
+
+        static int n;
+        static int[] numbers;
+        static int[] operators;
+
+        static void Main(string[] args)
+        {
+            n = Convert.ToInt32(Console.ReadLine());
+            numbers = Console.ReadLine().Split(' ').Select(token => Convert.ToInt32(token)).ToArray();
+            operators = Console.ReadLine().Split(' ').Select(token => Convert.ToInt32(token)).ToArray();
+
+            Node root = new Node() { level = 0, value = numbers[0], remainOperators = operators };
+            int max = root.FindMax();
+            int min = root.FindMin(); // getMin(n, numbers, operators);
+
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+
+        public class Node
+        {
+            public int level;       // current number index
+            public int value;       // result until current number
+            public int[] remainOperators;
+
+            internal int FindMax()
+            {
+                if (level == n - 1)
+                    return value;
+
+                // get child
+                List<Node> childs = new List<Node>();
+                for (int i = 0; i < (int)Operator.OperatorCounts; i++)
+                {
+                    if (remainOperators[i] > 0)
+                    {
+                        var nextRemain = remainOperators.ToArray();
+                        nextRemain[i]--;
+                        Node child = new Node()
+                        {
+                            level = level + 1,
+                            value = calculate(value, (Operator)i, numbers[level + 1]),
+                            remainOperators = nextRemain
+                        };
+
+                        childs.Add(child);
+                    }
+                }
+                return childs.Max(c => c.FindMax());
+            }
+
+            private int calculate(int value, Operator i, int v)
+            {
+                switch (i)
+                {
+                    case Operator.Plus:
+                        return value + v;
+                    case Operator.Minus:
+                        return value - v;
+                    case Operator.Multply:
+                        return value * v;
+                    case Operator.Divide:
+                        return value / v;
+                }
+                return -1;
+            }
+
+            internal int FindMin()
+            {
+                if (level == n - 1)
+                    return value;
+
+                // get child
+                List<Node> childs = new List<Node>();
+                for (int i = 0; i < (int)Operator.OperatorCounts; i++)
+                {
+                    if (remainOperators[i] > 0)
+                    {
+                        var nextRemain = remainOperators.ToArray();
+                        nextRemain[i]--;
+                        Node child = new Node()
+                        {
+                            level = level + 1,
+                            value = calculate(value, (Operator)i, numbers[level + 1]),
+                            remainOperators = nextRemain
+                        };
+
+                        childs.Add(child);
+                    }
+                }
+                return childs.Min(c => c.FindMin());
+            }
+        }
+
+    }
+}
---
+++ Baekjoon_2381_maximum_length/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon_2381_maximum_length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            Tuple<int, int> min = null;
+            Tuple<int, int> max = null;
+
+            Tuple<int, int> current;
+            for (int i=0;i<N;i++)
+            {
+                var input = Console.ReadLine().Split(' ').Select(token => Convert.ToInt32(token)).ToList();
+
+                current = new Tuple<int, int>(input[0] + 1000000, input[1] + 1000000);
+
+                min = (min == null) ? current : new Tuple<int, int>(Math.Max(min.Item1, current.Item1), Math.Min(min.Item2, current.Item2));
+                max = (max == null) ? current : new Tuple<int, int>(Math.Min(max.Item1, current.Item1), Math.Max(max.Item2, current.Item2));
+            }
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+
+        }
+    }
+}
